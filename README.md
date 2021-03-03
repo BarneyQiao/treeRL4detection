06852ddb40cbb6d6ead3a609b0ec64368a6f0453
Barney Qiao <tsqiaoliang@gmail.com>
2021-03-03 09:06:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# treeRL4detection
+ 
---
+++ rl.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Time          : 2021/3/2 15:43
+@Author        : BarneyQ
+@File          : rl.py
+@Software      : PyCharm
+@Description   :
+@Modification  :
+    @Author    :
+    @Time      :
+    @Detail    :
+
+"""
+
+class DQN(object):
+    def __init__(self, a_dim, s_dim ):
+        pass
+
+    def choose_action(self, s):
+        pass
+
+    def learn(self):
+        pass
+
+    def store_transition(self, s, a, r, s_):
+        pass
